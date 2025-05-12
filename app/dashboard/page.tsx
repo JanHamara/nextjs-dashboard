38a6a89098b8debe12261b7904d3ef55007d8ef6
Jan Hamara <hamara.jan18@gmail.com>
2025-05-12 20:08:48 +0800

    add next-boilerplate

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import { DocumentArrowDownIcon, UserIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+
+export default function Page() {
+  return (<>
+    <p className={'text-blue-950 font-bold text-lg'}>Dashboard Page</p>
+  </>);
+}
